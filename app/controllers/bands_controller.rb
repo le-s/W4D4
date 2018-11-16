3784ac073e4dd7126277ccb4d6f0c8caa6a002ae
Steven Le <steven.an.le@gmail.com>
2018-11-15 19:27:10 -0800

    Create bands table, controller and views

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,27 @@
+class BandsConroller < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+end
